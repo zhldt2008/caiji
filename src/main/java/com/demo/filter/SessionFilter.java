6a820ed2f5e8f7a1d47a61dd423309aed63341d0
zhou.hongliang <yuluo.zhl@dtyunxi.com>
2018-06-15 15:00:55 +0800

    增加过滤和拦截

---
+++ src/main/java/com/demo/filter/SessionFilter.java
@@ -0,0 +1,83 @@
+package com.demo.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SessionFilter implements Filter {
+
+	
+
+    /**
+     * 封装，不需要过滤的list列表
+     */
+    protected static List<Pattern> patterns = new ArrayList<Pattern>();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        String url = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
+        if (url.startsWith("/") && url.length() > 1) {
+            url = url.substring(1);
+        }
+
+        if (isInclude(url)){
+            chain.doFilter(httpRequest, httpResponse);
+            return;
+        } else {
+            HttpSession session = httpRequest.getSession();
+            if (session.getAttribute("") != null){
+                // session存在
+                chain.doFilter(httpRequest, httpResponse);
+                return;
+            } else {
+                // session不存在 准备跳转失败
+                /* RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+                    dispatcher.forward(request, response);*/
+            	httpResponse.sendRedirect("login");
+                return;
+            }
+        }
+
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+
+    /**
+     * 是否需要过滤
+     * @param url
+     * @return
+     */
+    private boolean isInclude(String url) {
+        for (Pattern pattern : patterns) {
+            Matcher matcher = pattern.matcher(url);
+            if (matcher.matches()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/demo/interceptor/LoginHandlerInterceptor.java
@@ -0,0 +1,31 @@
+package com.demo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * 
+ * @author zhou.hongliang
+ *登录过滤器实现
+ */
+public class LoginHandlerInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	
+
+}
