6310a4fa76fed757325e8e6cd3bbff52156a4f1a
zhou.hongliang <yuluo.zhl@dtyunxi.com>
2018-06-15 14:59:58 +0800

    增加过滤和拦截功能

--- src/main/java/com/demo/config/WebMvcConfig.java
+++ src/main/java/com/demo/config/WebMvcConfig.java
@@ -1,8 +1,16 @@
 package com.demo.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.demo.filter.SessionFilter;
+import com.demo.interceptor.LoginHandlerInterceptor;
 
 
 
@@ -14,4 +22,40 @@ public class WebMvcConfig extends WebMvcConfigurationSupport  {
 	   public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {  
 	       configurer.enable();  
 	   }  
+	
+	 /**
+     * 添加拦截器
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                //添加需要验证登录用户操作权限的请求
+                .addPathPatterns("/*")
+                //排除不需要验证登录用户操作权限的请求
+                .excludePathPatterns("/test/*");
+    }
+    
+    
+    /**
+     * 配置过滤器
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean someFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(sessionFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("sessionFilter");
+        return registration;
+    }
+
+    /**
+     * 创建一个bean
+     * @return
+     */
+    @Bean(name = "sessionFilter")
+    public Filter sessionFilter() {
+        return new SessionFilter();
+    }
 }
